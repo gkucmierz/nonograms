9e8725d48bbbfee6e64c71392586b7fb2d4b8117
Grzegorz Kucmierz <gkucmierz@gmail.com>
2026-02-08 17:19:04 +0100

    PWA: manifest i service worker

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,41 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Nonograms',
+        short_name: 'Nonograms',
+        description: 'Nonograms',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#191654',
+        theme_color: '#00f2fe',
+        icons: [
+          {
+            src: '/pwa-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/pwa-512x512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
